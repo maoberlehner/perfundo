fc7f0b8ee5408c8ef76437c59f97668c4a9eb404
Markus Oberlehner <markus.oberlehner@gmail.com>
2014-12-05 17:00:46 +0100

    established basic functionality

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('styles', function () {
 
 // Minify
 gulp.task('minify', ['styles'], function () {
-  return gulp.src('dist/**/*.css')
+  return gulp.src('dist/perfundo.css')
     .pipe(minifyCSS())
     .pipe(cssshrink())
     .pipe(rename(function (path) {
