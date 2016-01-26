3612f1f68fe476db48da1595847c27f9bf936b45
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-01-26 17:28:28 +0100

    revamped the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,73 @@
-// Load plugins
+/**
+ * Configuration
+ */
+var config = {
+  destination: 'dist',
+  watchFiles: ['src/**/*.scss']
+};
+
+/**
+ * Plugins
+ */
+var del          = require('del');
 var gulp         = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
-var cssshrink    = require('gulp-cssshrink');
+var cssnano      = require('gulp-cssnano');
 var livereload   = require('gulp-livereload');
-var minifyCSS    = require('gulp-minify-css');
+var minifyCss    = require('gulp-minify-css');
 var rename       = require('gulp-rename');
-var sass         = require('gulp-ruby-sass');
-var gutil        = require('gulp-util');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
 
-// Styles
-gulp.task('styles', function () {
-  return gulp.src('src/**/*.scss')
-    .pipe(sass({ style: 'compact', precision: 7, sourcemap: true }))
-    .on('error', gutil.log)
-    .pipe(autoprefixer('last 2 versions'))
-    .on('error', gutil.log)
-    .pipe(gulp.dest('dist'));
-});
-
-// Minify
-gulp.task('minify', ['styles'], function () {
-  return gulp.src(['dist/perfundo.css', 'dist/perfundo-icons.css'])
-    .pipe(minifyCSS())
-    .pipe(cssshrink())
-    .pipe(rename(function (path) {
-      path.basename += '.min';
-    }))
-    .pipe(gulp.dest('dist'))
+/**
+ * Styles
+ */
+gulp.task('styles:build', ['clean:styles'], function () {
+  return gulp.src(config.watchFiles)
+    .pipe(sourcemaps.init())
+      .pipe(sass({ precision: 7, errLogToConsole: true }))
+      .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.destination))
     .pipe(livereload());
 });
 
-// Watch
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.scss', ['styles', 'minify']);
+gulp.task('styles:minify', ['styles:build'], function () {
+  return gulp.src(config.destination + '/*.css')
+    .pipe(minifyCss())
+    .pipe(cssnano())
+    .pipe(rename(function (path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest(config.destination))
+    .pipe(livereload());
 });
 
-// Default
+/**
+ * Clean
+ *
+ * Remove compiled files before regenerating them.
+ */
+gulp.task('clean:styles', function () {
+  return del([
+    // Remove everything inside the destination directory.
+    config.destination + '/**/*'
+  ]);
+});
+
+/**
+ * Watch
+ */
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(config.watchFiles, ['styles:minify']);
+});
+
+/**
+ * Default task
+ *
+ * Run this task with `gulp`.
+ */
 gulp.task('default', function () {
   gulp.start('watch');
 });
