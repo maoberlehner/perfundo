61858d69a1eb0eec6487b45d01c1d53bcda8439f
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-04-02 17:38:39 +0200

    add @TODO

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,2 +1,2 @@
 var perfundo = require('../../js/index.js'); // @TODO: load from noe modules.
-var perfundo = new perfundo('.perfundo', { disableHistory: true });
+var perfundo = new perfundo('.perfundo', { disableHistory: true }); // @TODO: remove options.
