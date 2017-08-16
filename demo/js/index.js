346d69e1a8efc230956c644ebdf33ebd4b2a225f
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-08-16 17:49:36 +0200

    Update the demo
    - Use one build process.
    - Use latest avalanche.

--- demo/js/index.js
+++
@@ -1,8 +0,0 @@
-// Load the module.
-// @TODO: load from node modules.
-import Perfundo from '../../js/index.js';
-// Initialize a perfundo Lightbox.
-const myLightbox = new Perfundo('.perfundo-js', {
-  disableHistory: true,
-  swipe: true
-});
