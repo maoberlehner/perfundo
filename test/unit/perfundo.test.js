2666eca2966106bef3f4e552616d1403ceabdeb2
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-09-01 05:12:14 +0200

    Add missing unit tests
    Fixes #96

--- test/unit/perfundo.test.js
+++ test/unit/perfundo.test.js
@@ -39,3 +39,70 @@ test(`Returns an array of Perfundo instances if a multi target is given.`, (t) =
   t.true(typeof perfundoInstances[0] === `object`);
   t.true(typeof perfundoInstances[1] === `object`);
 });
+
+test(`Should add and remove active class on overlay element.`, (t) => {
+  const context = createContext(defaultOptions, 1);
+  const dependencies = Object.assign({}, defaultDependencies, { context });
+  const perfundoInstance = new Perfundo(dependencies, `.perfundo`);
+
+  perfundoInstance.open();
+  let overlayElementHasActiveClass = context.querySelector(`.perfundo__overlay`).classList.contains(`is-active`);
+
+  t.true(overlayElementHasActiveClass);
+
+  perfundoInstance.close();
+  overlayElementHasActiveClass = context.querySelector(`.perfundo__overlay`).classList.contains(`is-active`);
+
+  t.false(overlayElementHasActiveClass);
+});
+
+test(`Should call \`close()\`, and \`click()\` on the previous overlay element.`, (t) => {
+  const context = createContext(defaultOptions, 2);
+  const dependencies = Object.assign({}, defaultDependencies, { context });
+
+  let mockCloseCalled = false;
+  const mockClose = () => { mockCloseCalled = true; };
+
+  let mockClickCalled = false;
+  const mockClick = () => { mockClickCalled = true; };
+
+  const perfundoInstances = new Perfundo(dependencies, `.perfundo`);
+
+  perfundoInstances[1].close = mockClose;
+  context.querySelector(`.perfundo__link`).click = mockClick;
+  perfundoInstances[1].prev();
+
+  t.true(mockCloseCalled);
+  t.true(mockClickCalled);
+});
+
+test(`Should call \`close()\`, and \`click()\` on the next overlay element.`, (t) => {
+  const context = createContext(defaultOptions, 2);
+  const dependencies = Object.assign({}, defaultDependencies, { context });
+
+  let mockCloseCalled = false;
+  const mockClose = () => { mockCloseCalled = true; };
+
+  let mockClickCalled = false;
+  const mockClick = () => { mockClickCalled = true; };
+
+  const perfundoInstances = new Perfundo(dependencies, `.perfundo`);
+
+  perfundoInstances[0].close = mockClose;
+  context.querySelector(`.perfundo + .perfundo .perfundo__link`).click = mockClick;
+  perfundoInstances[0].next();
+
+  t.true(mockCloseCalled);
+  t.true(mockClickCalled);
+});
+
+test(`Should return the elements root Perfundo element.`, (t) => {
+  const context = createContext(defaultOptions, 1);
+  const dependencies = Object.assign({}, defaultDependencies, { context });
+  const perfundoInstance = new Perfundo(dependencies, `.perfundo`);
+
+  const actual = perfundoInstance.getRootElement(context.querySelector(`.perfundo__overlay`));
+  const expected = context.querySelector(`.perfundo`);
+
+  t.deepEqual(actual, expected);
+});
