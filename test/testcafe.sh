e1d1f181dac3d0612f789273aa1d761242169c2f
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-10-30 05:39:01 +0100

    Run BrowserStack tests on latest browsers only
    Specifying browser versions has the downside that they become old pretty
    quickly. It is better to test only on latest than to test on old
    versions which nobody is using anymore.

--- test/testcafe.sh
+++ test/testcafe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-browsers=( "browserstack:ie@10.0:Windows 8" "browserstack:ie@11.0:Windows 10" "browserstack:edge@15.0:Windows 10" "browserstack:edge@14.0:Windows 10" "browserstack:firefox@54.0:Windows 10" "browserstack:firefox@55.0:Windows 10" "browserstack:chrome@59.0:Windows 10" "browserstack:chrome@60.0:Windows 10" "browserstack:opera@46.0:Windows 10" "browserstack:opera@47.0:Windows 10" "browserstack:safari@9.1:OS X El Capitan" "browserstack:safari@10.1:OS X Sierra" )
+browsers=( "browserstack:ie@10.0:Windows 8" "browserstack:ie@11.0:Windows 10" "browserstack:edge:Windows 10" "browserstack:firefox:Windows 10" "browserstack:chrome:Windows 10" "browserstack:opera:Windows 10" "browserstack:safari:OS X High Sierra" )
 
 for i in "${browsers[@]}"
 do
